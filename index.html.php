b48c20058d8767b90c12a7d9e3b268047614ff04
Alexander <alexander@dpi.nl>
2017-02-06 15:15:47 +0100

    Silly meta.

--- index.html.php
+++ index.html.php
@@ -2,7 +2,11 @@
 <html dir="ltr" lang="en">
 <head>
     <meta charset="utf-8">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <meta name="description" content="Build sophisticated layouts out of the box.">
+    <meta name="theme-color" content="#f43530">
+    <meta name="viewport" content="initial-scale=1.0,shrink-to-fit=no,width=device-width">
     <meta property="fb:app_id" content="355615554837150">
     <meta property="og:url" content="https://grid.sexy/">
     <title>Grid.sexy</title>
--- tests/columns.html.php
+++ tests/columns.html.php
@@ -2,6 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <meta name="theme-color" content="#f43530">
+    <meta name="viewport" content="initial-scale=1.0,shrink-to-fit=no,width=device-width">
     <meta property="fb:app_id" content="355615554837150">
     <meta property="og:url" content="https://grid.sexy/tests/columns">
     <title>Test</title>
