8bdda68276bc89111b3cb9200e768b9054f30887
acj <gitlab@acjs.net>
2017-02-06 23:59:27 +0100

    Rows test.

---
+++ tests/rows.html.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="theme-color" content="#f43530">
+    <meta name="viewport" content="initial-scale=1.0,shrink-to-fit=no,width=device-width">
+    <meta property="fb:app_id" content="355615554837150">
+    <meta property="og:url" content="https://grid.sexy/tests/rows">
+    <title>Rows</title>
+    <link rel="author" href="https://alexanderchristiaanjacob.com/">
+    <link rel="canonical" href="https://grid.sexy/tests/rows">
+    <link rel="stylesheet" href="../css/grid.css">
+    <link rel="stylesheet" href="main.css">
+    <style>
+
+    </style>
+</head>
+<body id="top">
+<div class="container">
+    <div class="row">
+        <div class="col-xs-1o1">row</div>
+    </div>
+    <div class="row no-gutter">
+        <div class="col-xs-1o1">row no-gutter</div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-xs-1o1">row</div>
+    </div>
+    <div class="row no-gutter">
+        <div class="col-xs-1o1">row no-gutter</div>
+    </div>
+</div>
+</body>
+</html>
