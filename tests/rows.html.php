2c05b4bb75acb488988f08109abd2b7536f2acb8
acj <gitlab@acjs.net>
2017-02-09 18:17:43 +0100

    Rows test design.

--- tests/rows.html.php
+++ tests/rows.html.php
@@ -13,25 +13,44 @@
     <link rel="stylesheet" href="main.css">
     <style>
 
+        h1 {
+            text-align: center;
+        }
+
+        html {
+            background: #0f0a01;
+            color: #eaded0;
+        }
+
+        pre {
+            padding-bottom: .75rem;
+            padding-top: .75rem;
+        }
+
+        .demo {
+            background: #dbb700;
+            color: #0f0a01;
+            outline: none;
+        }
+
+        .demo * {
+            background: transparent;
+            outline: none;
+        }
+
     </style>
 </head>
 <body id="top">
 <h1>Rows</h1>
 <div class="container">
-    <div class="row">
-        <div class="col-xs-1o1">row</div>
+    <div class="demo row">
+        <div class="col">row</div>
     </div>
-    <div class="row no-gutter">
+    <pre><code class="html">&lt;div class="row">&lt;/div></code></pre>
+    <div class="demo row no-gutter">
         <div class="col-xs-1o1">row no-gutter</div>
     </div>
-</div>
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-xs-1o1">row</div>
-    </div>
-    <div class="row no-gutter">
-        <div class="col-xs-1o1">row no-gutter</div>
-    </div>
+    <pre><code class="html">&lt;div class="row no-gutter">&lt;/div></code></pre>
 </div>
 </body>
 </html>
