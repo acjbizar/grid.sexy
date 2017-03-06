b0eef34387f3d7e22e0e0b54fd87e45961d8900e
acj <gitlab@acjs.net>
2017-03-06 22:08:37 +0100

    New home test.

---
+++ tests/home.html.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Grid.sexy</title>
+    <link rel="stylesheet" href="../css/grid.min.css">
+    <style>
+
+        html {
+            font: normal 100%/1.5 sans-serif, serif;
+        }
+
+    </style>
+</head>
+<body id="top">
+<main id="content">
+    <h1>Grid.sexy</h1>
+    <section id="columns">
+        <h2>Columns</h2>
+        <pre></pre>
+    </section>
+    <section id="rows">
+        <h2>Rows</h2>
+        <pre></pre>
+    </section>
+    <section id="containers">
+        <h2>Containers</h2>
+        <pre></pre>
+    </section>
+</main>
+</body>
+</html>
